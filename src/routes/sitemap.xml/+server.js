837032b91e9bb6376314789af8cac08e87364a7a
FabienBounoir <bounoirfabien@gmail.com>
2024-11-02 11:41:24 +0100

    fix: 🐛 Site Map

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -13,31 +13,31 @@ export async function GET() {
 			<url>
                 <loc>https://anotherpp.vercel.app/</loc>
                 <lastmod>2024-11-01T18:30:00+00:00</lastmod>
-                <priority>1</priority
+                <priority>1</priority>
             </url>
 
             <url>
                 <loc>https://anotherpp.vercel.app/create</loc>
                 <lastmod>2024-11-01T18:30:00+00:00</lastmod>
-                <priority>0.9</priority
+                <priority>0.9</priority>
             </url>
 
             <url>
                 <loc>https://anotherpp.vercel.app/join</loc>
                 <lastmod>2024-11-01T18:30:00+00:00</lastmod>
-                <priority>0.8</priority
+                <priority>0.8</priority>
             </url>
 
             <url>
                 <loc>https://anotherpp.vercel.app/manager</loc>
                 <lastmod>2024-11-01T18:30:00+00:00</lastmod>
-                <priority>0.7</priority
+                <priority>0.7</priority>
             </url>
 
             <url>
                 <loc>https://anotherpp.vercel.app/rooms</loc>
                 <lastmod>2024-11-01T18:30:00+00:00</lastmod>
-                <priority>0.7</priority
+                <priority>0.7</priority>
             </url>
 		</urlset>`.trim(),
         {
